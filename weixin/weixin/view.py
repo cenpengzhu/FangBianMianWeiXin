0469088fecaa2b87320305448e20232a97a9487a
cenpenzhu <cenpeng.zhu@gmail.com>
2019-04-03 12:08:06 +0800

    修复验证错误
    修复验证错误

--- weixin/weixin/view.py
+++ weixin/weixin/view.py
@@ -18,13 +18,13 @@ def hello(request):
 def AcessConfirm(request):
     logger.info('AcessConfirmBegin')
     if request.method=='GET':
-        SignNature = request.GET.get('signature',default='')
+        SignNature = request.GET.get('signature',default='52b4d31088afc0a3aeb285ca96a90925282695a6')
         logger.info('SignNature get is '+SignNature)
-        TimeStamp = request.GET.get('timestamp',default='')
+        TimeStamp = request.GET.get('timestamp',default='1554263502')
         logger.info('TimeStamp get is '+TimeStamp)
-        Nonce = request.GET.get('nonce',default = '')
+        Nonce = request.GET.get('nonce',default = '1480077730')
         logger.info('Nonce get is '+Nonce)
-        EchoStr = request.GET.get('echostr',default='')
+        EchoStr = request.GET.get('echostr',default='4699500745244225535')
         logger.info('EchoStr get is '+EchoStr)
         if SignNature == '' or TimeStamp == '' or Nonce == '' or EchoStr == '' :
             logger.info('Confire failed wrong param!')
@@ -34,11 +34,15 @@ def AcessConfirm(request):
         StrList = [Token,TimeStamp,Nonce]
         StrList.sort()
         sha1 = hashlib.sha1()
-        map(sha1.update,StrList)
+        sha1.update("".join(StrList).encode("utf8"))
         HashResult = sha1.hexdigest()
+        logger.info('HashResult is '+HashResult)
 
         if HashResult == SignNature :
+            logger.info('Confirm success')
             return HttpResponse(EchoStr)
+        else :
+            return HttpResponse('Confirm failed')
         
 
 
