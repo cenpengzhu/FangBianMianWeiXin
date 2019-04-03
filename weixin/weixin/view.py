da3468d1c008749283883d1899760e79fb4555b8
cenpenzhu <cenpeng.zhu@gmail.com>
2019-04-03 11:48:56 +0800

    增加日志调试
    增加日志调试

--- weixin/weixin/view.py
+++ weixin/weixin/view.py
@@ -1,16 +1,33 @@
 from django.http import HttpResponse
 import hashlib
+import logging
+
+logger = logging.getLogger('httplogger')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('djangoviews.log')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
 
 def hello(request):
+    logger.info('Hello wordl!')
     return HttpResponse("Hello wordl!")
 
 def AcessConfirm(request):
+    logger.info('AcessConfirmBegin')
     if request.method=='GET':
         SignNature = request.GET.get('signature',default='')
+        logger.info('SignNature get is '+SignNature)
         TimeStamp = request.GET.get('timestamp',default='')
+        logger.info('TimeStamp get is '+TimeStamp)
         Nonce = request.GET.get('nonce',default = '')
+        logger.info('Nonce get is '+Nonce)
         EchoStr = request.GET.get('echostr',default='')
+        logger.info('EchoStr get is '+EchoStr)
         if SignNature == '' or TimeStamp == '' or Nonce == '' or EchoStr == '' :
+            logger.info('Confire failed wrong param!')
             return HttpResponse('wrong param!')
         Token = 'tokenofnazhefangbianmian'
 
